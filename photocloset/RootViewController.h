a887d9e05774a835a6e98d89fccebaa2b3042177
Stephen Burke <sburke@macbookpro.local>
2011-06-03 19:49:11 -0500

    Initial Commit

---
+++ photocloset/RootViewController.h
@@ -0,0 +1,20 @@
+//
+//  RootViewController.h
+//  photocloset
+//
+//  Created by Stephen Burke on 6/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+
+}
+
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+
+@end
